78f3630896b6479b0fb99c5f4a367a88ccb5b3f3
sinsinpurin <tusanch6@gmail.com>
2021-08-11 17:21:47 +0900

    feat: フロントとバックのアウトラインの実装

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Mock</div>;
+  const [did, setDid] = React.useState("");
+  const [vc, setVc] = React.useState("");
+  const [isVerified, setIsVerified] = React.useState(false);
+
+  React.useEffect(() => {
+    createDid();
+  }, []);
+
+  // TODO: Did作る
+  const createDid = () => {
+    setDid("did:ethr:0xB1A25D6E37ad12579801eBb6787636fd63ba87cc");
+  };
+
+  const issue = async () => {
+    const response = await axios.post("http://localhost:5001/blockbase-vcmock-prod/us-central1/issue", { did });
+
+    setVc(response.data.vc);
+  };
+
+  const verify = async () => {
+    const response = await axios.post("http://localhost:5001/blockbase-vcmock-prod/us-central1/verify", { vc });
+    setIsVerified(response.data.result);
+  };
+
+  return (
+    <div className="App">
+      <h2>DID</h2>
+      <p>{did}</p>
+      <h2>VCの発行・検証</h2>
+      <button onClick={issue}>発行</button>
+      <button onClick={verify}>検証</button>
+      <h2>VCの表示</h2>
+      {vc && <img src={vc} alt="vc" />}
+      <h2>VCの検証結果</h2>
+      <p>{isVerified && "ok"}</p>
+    </div>
+  );
 }
 
 export default App;
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,14 +1,24 @@
 import * as functions from "firebase-functions";
+import { vc } from "./utils";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const cors = require("cors")({ origin: true });
 
+// TODO: 受け取ったDIDに対してvcを発行
+// vcを作成してimageにOpenBadges形式でbakeする
+// Base64で画像を返す
 export const issue = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    console.log(request.body.did);
+    response.send({ vc });
+  });
 });
 
+// TODO: 受け取ったBase64からiTxTをとってきてVCの検証
+// 検証結果をResultとして返す
 export const verify = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    const result = true;
+    response.send({ result });
+  });
 });
